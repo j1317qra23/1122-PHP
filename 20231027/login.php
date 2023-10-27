04c2c4c8afc8e17c48ad8e82d0a3bda0cdffb381
Jason Tsai <qrass06040130@gmail.com>
2023-10-27 16:21:43 +0800

    login

--- 20231027/login.php
+++ 20231027/login.php
@@ -1,27 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>會員登入</title>
     <style>
         .login-block{
-            padding: 30px 40px;
-            border: 1px solid #999;
+            padding:30px 40px;
+            border:1px solid #999;
             box-shadow: 2px 2px 15px #999;
-            margin: 10px auto;
-            width: 280px;
+            margin:10px auto;
+            width:280px;
         }
-        .login-input{
+        .login-input {
             margin: 10px;
         }
         .login-input input[type='text']{
-            border: 1px solid #999;
+            border:1px solid blue;
         }
         .login-input input{
             font-size:20px;
-            padding: 5px;
+            padding:5px;
         }
         .btn{
             text-align: center;
@@ -36,33 +35,36 @@
         }
     </style>
 </head>
-
 <body>
-<div class="login-block">
+<div class='login-block'>
     <?php
-if(isset($_GET['m'])){
-    echo "<span style='color:red'>".$_GET['m']."</span>";
-}
+    if(isset($_GET['m'])){
+        echo "<span style='color:red'>".$_GET['m']."</span>";
+    }
+    
+    if(isset($_GET['login']) && $_GET['login']==1){
+        echo "歡迎你";
+    }else{
+?>
+<form action="check.php" method="post">
+        <div class='login-input'>
+            <label for="acc">帳號:</label>
+            <input type="text" name="acc" id="acc">
+        </div>
+        <div class='login-input'>
+            <label for="pw">密碼:</label>
+            <input type="password" name="pw" id="pw">
+        </div>
+        <div class='btn'>
+            <input type="submit" value="登入">
+            <input type="reset" value="重置">
+        </div>
+    </form>
+<?php
+    }
 
     ?>
-<form action="check.php" method="post">
-  
-    <div class=login-input>
-        <label for="acc">帳號</label>
-        <input type="text" name="acc" id="acc">
-    </div>
-   
-    <div class=login-input>
-        <label for="password">密碼</label>
-        <input type="password" name="pw" id="pw">
-    </div>
-    <div class='btn'>
-        <input type="submit" value="登入">
-        <input type="reset" value="重置">
-    </div>
-    </div>
-</form>
+    
+</div>    
 </body>
-
 </html>
-
